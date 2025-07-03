cc0af864e0cf8c1337ae7cb779cd9bcad3021aae
Sasterr1 <farhanardianur@gmail.com>
2025-07-03 18:14:04 +0800

    UAS CONNECTAPP

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center h-[100vh]">
       <div className="flex flex-col border-2 gap-3 items-center p-8 rounded-xl w-[420px]">
-        <p className="text-2xl font-bold text-center">CONNECT WITH US</p>
+        <p className="text-2xl font-bold text-center">CONNECT WITH US!</p>
         <Input placeholder="Masukan Email" type="Email" onChange={(e) => setData((currentData) => ({...currentData, username: e.target.value,}))}/>
 
         <Input className="w-[350px]" placeholder="Masukan Password" type="Password" 
